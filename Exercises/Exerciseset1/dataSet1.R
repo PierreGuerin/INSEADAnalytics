d73566a35b9ceb35d8276c7f8eb24bbdba724ec0
Pierre Guérin <Pierre Gurin>
2017-01-20 18:29:26 +0800

    update

--- Exercises/Exerciseset1/dataSet1.R
+++ Exercises/Exerciseset1/dataSet1.R
@@ -1,6 +1,6 @@
-sourc
+
 # rm(list=ls()) # Clean up the memory, if we want to rerun from scratch
-# source("helpersSet1.R")
+source("helpersSet1.R")
 
 getdata.fromscratch = 1
 
@@ -9,10 +9,6 @@ mytickers = c("SPY", "AAPL","GOOG")  # Other tickers for example are "GOOG", "GS
 startDate = "2001-01-01"
 
 if (getdata.fromscratch){
-  # Get SPY first, to get all trading days
-  tmp<-as.matrix(try(getSymbols(Symbols="SPY",from = startDate,src = website_used, auto.assign=FALSE)))
-  StockPrices=matrix(rep(0,nrow(tmp)*length(mytickers)), ncol=length(mytickers))
-  colnames(StockPrices)<-mytickers; 
   rownames(StockPrices)<-rownames(tmp)
   StockVolume=StockPrices
   StockPrices[,1] <- tmp[,6]
